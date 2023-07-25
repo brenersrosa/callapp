2e7c6795664fcd49a2ddeab43fd5c1e6f0906b89
Brener Rosa <brenersrosa@gmail.com>
2023-07-25 02:10:51 -0300

    feat: Create register

---
+++ src/components/register/MultiStep.tsx
@@ -0,0 +1,32 @@
+import clsx from 'clsx'
+
+interface MultiStepProps {
+  size: number
+  currentStep?: number
+}
+
+export function MultiStep({ size, currentStep = 1 }: MultiStepProps) {
+  return (
+    <div className="px-6">
+      <span className="text-xs text-zinc-200">
+        Passo {currentStep} de {size}
+      </span>
+
+      <div
+        className="mt-1 grid gap-2"
+        style={{ gridTemplateColumns: `repeat(${size}, 1fr)` }}
+      >
+        {Array.from({ length: size }, (_, i) => i + 1).map((step) => {
+          return (
+            <div
+              key={step}
+              className={clsx('h-1 rounded-full bg-zinc-600', {
+                'bg-gray-100': currentStep >= step,
+              })}
+            ></div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,127 @@
+import { useEffect } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { ArrowRight, X } from 'lucide-react'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { AxiosError } from 'axios'
+import colors from 'tailwindcss/colors'
+import { Toaster, toast } from 'react-hot-toast'
+
+import { MultiStep } from '@/components/register/MultiStep'
+import { Box } from '@/components/global/Box'
+import { Input } from '@/components/global/Input'
+import { Button } from '@/components/global/Button'
+
+import { api } from '@/lib/axios'
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
+    })
+    .transform((username) => username.toLocaleLowerCase()),
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisa ter pelo menos 3 caracteres.' }),
+})
+
+type RegisterFormData = z.infer<typeof registerFormSchema>
+
+export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  })
+
+  const router = useRouter()
+
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    if (searchParams.get('username')) {
+      setValue('username', String(searchParams.get('username')))
+    }
+  }, [searchParams, setValue])
+
+  async function handleRegister(data: RegisterFormData) {
+    try {
+      await api.post('/users', {
+        username: data.username,
+        name: data.name,
+      })
+
+      await router.push('/register/connect-calendar')
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data) {
+        console.log(err.response.data)
+        toast.error('Este usuário já existe', {
+          position: 'top-right',
+          style: {
+            backgroundColor: colors.red[500],
+            color: colors.white,
+            fontSize: 16,
+            fontWeight: 500,
+            padding: 16,
+          },
+          icon: <X size={24} className="text-zinc-50" />,
+        })
+        return
+      }
+      console.log(err)
+    }
+  }
+
+  return (
+    <div className="mx-auto mb-4 mt-20 flex max-w-xl flex-col gap-6 px-4">
+      <Toaster />
+
+      <div className="flex flex-col gap-2 px-6">
+        <strong className="text-2xl font-bold leading-normal">
+          Bem-vindo ao Ignite Call!
+        </strong>
+
+        <span className="text-zinc-400">
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </span>
+      </div>
+
+      <MultiStep size={4} currentStep={1} />
+
+      <Box as="form" onSubmit={handleSubmit(handleRegister)}>
+        <Input
+          label="Nome de usuário"
+          prefix="ignite.com/"
+          placeholder="seu-usuario"
+          {...register('username')}
+          message="Informe o seu nome de usuário."
+          error={errors.username}
+          info
+          autoFocus
+        />
+
+        <Input
+          label="Nome completo"
+          placeholder="Seu nome"
+          {...register('name')}
+          message="Informe seu nome completo."
+          info
+          error={errors.name}
+        />
+
+        <Button
+          type="submit"
+          title="Próximo passo"
+          icon={<ArrowRight size={20} />}
+        />
+      </Box>
+    </div>
+  )
+}
